879ea24088d7209485d67fef49af0f7878d81f50
maxc0des <schneider.max16@yahoo.com>
2024-10-11 16:47:48 +0200

    redesinig the website

---
+++ robotics.website.release/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("wir arbeiten gerade noch an dieser website, vorerst findest du unsere website deshab noch unter https://robotics-rutesheim.netlify.app/")
+    console.log("hello world");
+    document.getElementById("img").addEventListener("click", script);
+
+    function script(){
+        alert("why did you click this?");
+    }
+});
