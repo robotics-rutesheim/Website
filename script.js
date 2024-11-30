c1797715906d850550054df48e6e692997b7fba6
Blockworldhd <107620354+BlockworldHD@users.noreply.github.com>
2024-11-30 20:27:27 +0100

    Changed JS to work locally

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", load_content);
 function load_content() {
     source = ["about", "competitions", "news"];
     for (let i = 0; i < source.length; i++) {
-        fetch(`/content/${source[i]}.txt`)
+        fetch(`./content/${source[i]}.txt`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Fehler beim Laden von ${source[i]}: ${response.statusText}`);
