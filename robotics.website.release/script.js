b22ed4608d8e640e1965d3add5b79f4741dfe27f
maxc0des <schneider.max16@yahoo.com>
2024-11-30 16:46:32 +0100

    updated layout and content

--- robotics.website.release/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("wir arbeiten gerade noch an dieser website, vorerst findest du unsere website deshab noch unter https://robotics-rutesheim.netlify.app/")
-    console.log("hello world");
-    document.getElementById("img").addEventListener("click", script);
-
-    function script(){
-        alert("why did you click this?");
-    }
-});
---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", load_content);
+
+function load_content() {
+    source = ["about", "competitions", "news"];
+    for (let i = 0; i < source.length; i++) {
+        fetch(`/content/${source[i]}.txt`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Fehler beim Laden von ${source[i]}: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(content => {
+            document.getElementById(`${source[i]}_content`).innerHTML = content;
+        })
+    }
+}
